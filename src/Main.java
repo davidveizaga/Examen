f21b34ecda637e89ffc2c6e88a6cb010aed3898c
david <qqqcruzito@gmail.com>
2023-10-12 09:55:02 -0400

    examen completado

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String args[]) {
+        Suma sum1 = new Suma();
+        sum1.calcularSuma(10,10);
+        Suma sum4= new Suma();
+        sum4.calcularSuma(10,10,10);
+        Suma sum2 = new Suma();
+        sum2.calcularSuma(10.5,10.5);
+        Suma sum3 = new Suma();
+        sum3.calcularSuma(10.5,10.5,10.5);
+    }
+
+}
